461767a8dd9ffe4be6771f760043deb23d071efd
ARMBG23 <aaronrbeltreg@gmail.com>
2024-07-07 12:21:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-#Iteracion sobre la lista de nombres. 
-#Que imprima solamente los nombres que tengan igual o más de tres vocales.
+dato = "oso, perro, 10, 5 son animales"
 
-vocales = ['a','e','i','o','u','A','E','I','O','U',
-   'á','é','í','ó','ú','Á','É','Í','Ó','Ú']
-nombre= input("Ingrese un nombre: ")
-    
-contador=0
-for letra in nombre:
-  if letra in vocales:
-    contador += 1
-if contador >= 3:
-  print('El nombre', nombre, 'tiene', contador, 'vocales')
-elif contador < 3: 
-  print('El nombre', nombre, 'no tiene 3 vocales')
+valores = dato.split(",")
 
-        
+animal1 = valores[0]
+animal2 = valores[1]
+cantidad_animal2= valores[2]
+cantidad_animal1= valores[3].split()[0]
+
+def es_palindromo(palabra):
+  return palabra == palabra [:: -1]
+
+if es_palindromo(animal1):
+    print(f"{animal1} es un palíndromo")
+else:
+    print(f"{animal1} no es un palíndromo")
+
+if es_palindromo (animal2):
+  print(f"{animal2} es un palíndromo")
+else:
+  print(f"{animal2} no es un palíndromo")
+
+print (f"tenemos {cantidad_animal1} {animal1.upper()}S")
+print (f"tenemos {cantidad_animal2} {animal2}s")
+print (f"{animal1} y {animal2} son animales")
